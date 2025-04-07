a4cedea956d6da008153614f0f3ffe8e870e1fc2
Abhishek Tiwari <131746064+Abhishektiwari6398@users.noreply.github.com>
2025-04-07 22:10:05 +0530

    Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import { animate, motion } from "framer-motion";
 import Typewriter from "typewriter-effect";
 import { BsArrowUpRight, BsChevronDown } from "react-icons/bs";
-import me from "../assets/12.png";
+import me from "../assets/1.png";
 
 const Home = () => {
   const clientCount = useRef(null);
